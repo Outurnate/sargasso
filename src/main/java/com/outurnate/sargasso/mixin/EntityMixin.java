de1b43757bd83176fa49033a2fefbd7724c816f2
Outurnate <joseph@outurnate.com>
2026-06-30 15:38:37 +0000

    auto commit 1.0.246

--- src/main/java/com/outurnate/sargasso/mixin/EntityMixin.java
+++ src/main/java/com/outurnate/sargasso/mixin/EntityMixin.java
@@ -1,12 +1,15 @@
 /* (C)2026 */
 package com.outurnate.sargasso.mixin;
 
+import com.mojang.logging.LogUtils;
 import com.outurnate.sargasso.loot.LostItemsSavedData;
 import com.outurnate.sargasso.registry.LocalDimensions;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
+
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,11 +17,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
     // The injection site for this is strange for a reason
     // ItemEntity doesn't override onBelowWord, so we
     // must inject at the parent
     @Inject(method = "onBelowWorld", at = @At("HEAD"))
     private void sargasso$onBelowWorld(CallbackInfo callbackInfo) {
+        LOGGER.debug("ALIVE");
         if ((Object) this instanceof ItemEntity self) {
             LostItemsSavedData.AddLostItem(self.getItem());
         } else if ((Object) this instanceof ServerPlayer self) {
