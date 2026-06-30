3140dfe011867c428c4e5694116e762ee4bf3181
Outurnate <joseph@outurnate.com>
2026-06-30 13:48:46 +0000

    auto commit 1.0.233

--- src/main/java/com/outurnate/sargasso/HeadExplosionEffect.java
+++ src/main/java/com/outurnate/sargasso/HeadExplosionEffect.java
@@ -1,15 +1,18 @@
 /* (C)2026 */
 package com.outurnate.sargasso;
 
+import com.mojang.logging.LogUtils;
 import com.outurnate.sargasso.registry.LocalMobEffects;
-
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import org.slf4j.Logger;
 
 public class HeadExplosionEffect extends MobEffect {
+    public static final Logger LOGGER = LogUtils.getLogger();
+
     public HeadExplosionEffect(MobEffectCategory category, int color) {
         super(category, color);
     }
@@ -17,7 +20,8 @@ public class HeadExplosionEffect extends MobEffect {
     @Override
     public boolean applyEffectTick(ServerLevel level, LivingEntity mob, int amplification) {
         MobEffectInstance selfInstance = mob.getEffect(LocalMobEffects.HEAD_EXPLOSION);
-        if (selfInstance.getDuration() < 10) {
+        LOGGER.debug(String.valueOf(selfInstance.getDuration()));
+        if (selfInstance != null && selfInstance.getDuration() < 10) {
             mob.hurtServer(level, mob.damageSources().magic(), 100000000.0F);
         }
         return true;
