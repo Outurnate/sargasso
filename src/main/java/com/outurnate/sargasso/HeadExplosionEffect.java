63dc711fdf0572c7da0eebb2dab1de74db8ce617
Outurnate <joseph@outurnate.com>
2026-06-30 13:52:06 +0000

    auto commit 1.0.234

--- src/main/java/com/outurnate/sargasso/HeadExplosionEffect.java
+++ src/main/java/com/outurnate/sargasso/HeadExplosionEffect.java
@@ -2,11 +2,9 @@
 package com.outurnate.sargasso;
 
 import com.mojang.logging.LogUtils;
-import com.outurnate.sargasso.registry.LocalMobEffects;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import org.slf4j.Logger;
 
@@ -19,11 +17,13 @@ public class HeadExplosionEffect extends MobEffect {
 
     @Override
     public boolean applyEffectTick(ServerLevel level, LivingEntity mob, int amplification) {
-        MobEffectInstance selfInstance = mob.getEffect(LocalMobEffects.HEAD_EXPLOSION);
-        LOGGER.debug(String.valueOf(selfInstance.getDuration()));
-        if (selfInstance != null && selfInstance.getDuration() < 10) {
-            mob.hurtServer(level, mob.damageSources().magic(), 100000000.0F);
-        }
+        mob.hurtServer(level, mob.damageSources().magic(), 100000000.0F);
         return true;
+    }
+
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int tickCount, int amplification) {
+        LOGGER.debug(String.valueOf(tickCount));
+        return tickCount < 10;
     }
 }
