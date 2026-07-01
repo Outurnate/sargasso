005cd730a315d4413630bc566485047b114e30e6
Outurnate <joseph@outurnate.com>
2026-07-01 14:49:48 +0000

    auto commit 1.0.276

--- src/main/java/com/outurnate/sargasso/block/GlitchBlock.java
+++ src/main/java/com/outurnate/sargasso/block/GlitchBlock.java
@@ -3,8 +3,19 @@ package com.outurnate.sargasso.block;
 
 import com.outurnate.sargasso.block.entity.GlitchBlockEntity;
 import com.outurnate.sargasso.registry.LocalBlockEntities;
+import com.outurnate.sargasso.registry.LocalBlocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -13,6 +24,9 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.Vec3;
+
 import org.jspecify.annotations.Nullable;
 
 public class GlitchBlock extends Block implements EntityBlock {
@@ -26,6 +40,57 @@ public class GlitchBlock extends Block implements EntityBlock {
 
     public GlitchBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected void entityInside(
+        BlockState state,
+        Level level,
+        BlockPos pos,
+        Entity entity,
+        InsideBlockEffectApplier effectApplier,
+        boolean isPrecise) {
+
+        if (entity instanceof LivingEntity livingEntity) {
+            float diameter = 8.0F;
+
+            double xx = livingEntity.getX() + (livingEntity.getRandom().nextDouble() - 0.5) * diameter;
+            double yy = Mth.clamp(
+                livingEntity.getY() + (livingEntity.getRandom().nextDouble() - 0.5) * diameter,
+                (double) level.getMinY(),
+                (double) (level.getMinY() + ((ServerLevel) level).getLogicalHeight() - 1));
+            double zz = livingEntity.getZ() + (livingEntity.getRandom().nextDouble() - 0.5) * diameter;
+            if (level.getBlockState(new BlockPos((int) xx, (int) yy, (int) zz))
+                .is(LocalBlocks.GLITCH.get())) {
+                return;
+            }
+
+            if (livingEntity.isPassenger()) {
+                livingEntity.stopRiding();
+            }
+
+            Vec3 oldPos = livingEntity.position();
+            if (livingEntity.randomTeleport(xx, yy, zz, false, ItemStack.EMPTY)) {
+                level.gameEvent(GameEvent.TELEPORT, oldPos, GameEvent.Context.of(livingEntity));
+                SoundSource soundSource;
+                SoundEvent soundEvent = SoundEvents.CHORUS_FRUIT_TELEPORT; // TODO CUSTOM
+                if (livingEntity instanceof Player) {
+                    soundSource = SoundSource.PLAYERS;
+                } else {
+                    soundSource = SoundSource.NEUTRAL;
+                }
+
+                level.playSound(
+                    null,
+                    livingEntity.getX(),
+                    livingEntity.getY(),
+                    livingEntity.getZ(),
+                    soundEvent,
+                    soundSource);
+                livingEntity.resetFallDistance();
+                livingEntity.resetCurrentImpulseContext();
+            }
+        }
     }
 
     @Override
