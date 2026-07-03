4c0cf1c990dc77a088163f878de9c616c6aa554f
Outurnate <joseph@outurnate.com>
2026-07-03 15:21:43 +0000

    auto commit 1.0.307

--- src/main/java/com/outurnate/sargasso/mixin/BlockEntityMixin.java
+++ src/main/java/com/outurnate/sargasso/mixin/BlockEntityMixin.java
@@ -2,22 +2,10 @@
 package com.outurnate.sargasso.mixin;
 
 import com.mojang.logging.LogUtils;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import net.minecraft.core.component.DataComponentPatch;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.SeededContainerLoot;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -38,31 +26,28 @@ public abstract class BlockEntityMixin {
         if ((Object) this instanceof ChiseledBookShelfBlockEntity self
             && level != null && level instanceof ServerLevel server) {
             try {
-                if (!self.components().has(DataComponents.CONTAINER_LOOT)) {
-                    return;
-                }
-
-                SeededContainerLoot containerLoot = self.components().get(DataComponents.CONTAINER_LOOT);
-
-                LootTable lootTable = server.getServer().reloadableRegistries()
-                    .getLootTable(containerLoot.lootTable());
-                LootParams params = new LootParams.Builder(server)
-                    .withParameter(LootContextParams.ORIGIN, self.getBlockPos().getCenter())
-                    .create(LootContextParamSets.CHEST);
-
-                self.clearContent();
-
-                int slot = 0;
-                List<Integer> slotMixer = IntStream.range(0, 6).boxed().collect(Collectors.toList());
-                Collections.shuffle(slotMixer);
-                for (ItemStack stack : lootTable.getRandomItems(params, containerLoot.seed())) {
-                    self.setItem(slotMixer.get(slot++), stack);
-                }
-
-                self.applyComponents(
-                    self.collectComponents(),
-                    DataComponentPatch.builder().remove(DataComponents.CONTAINER_LOOT).build());
-                self.setChanged();
+                /*
+                 * if (!self.components().has(DataComponents.CONTAINER_LOOT)) { return; }
+                 * 
+                 * SeededContainerLoot containerLoot =
+                 * self.components().get(DataComponents.CONTAINER_LOOT);
+                 * 
+                 * LootTable lootTable = server.getServer().reloadableRegistries()
+                 * .getLootTable(containerLoot.lootTable()); LootParams params = new
+                 * LootParams.Builder(server) .withParameter(LootContextParams.ORIGIN,
+                 * self.getBlockPos().getCenter()) .create(LootContextParamSets.CHEST);
+                 * 
+                 * self.clearContent();
+                 * 
+                 * int slot = 0; List<Integer> slotMixer = IntStream.range(0,
+                 * 6).boxed().collect(Collectors.toList()); Collections.shuffle(slotMixer); for
+                 * (ItemStack stack : lootTable.getRandomItems(params, containerLoot.seed())) {
+                 * self.setItem(slotMixer.get(slot++), stack); }
+                 * 
+                 * self.applyComponents( self.collectComponents(),
+                 * DataComponentPatch.builder().remove(DataComponents.CONTAINER_LOOT).build());
+                 * self.setChanged();
+                 */
             } catch (Exception e) {
                 LOGGER.error(e.toString());
                 throw e;
