50ca225d6538bd7948bd3ce62ad86881e1a71330
Outurnate <joseph@outurnate.com>
2026-07-12 15:47:36 +0000

    auto commit 1.0.390

--- src/main/java/com/outurnate/sargasso/client/HatClientExtensions.java
+++ src/main/java/com/outurnate/sargasso/client/HatClientExtensions.java
@@ -11,7 +11,7 @@ public class HatClientExtensions implements IClientItemExtensions {
         ItemStack itemStack,
         EquipmentClientInfo.LayerType layerType,
         Model original) {
-        System.out.println("Ass");
+        System.out.println(original.getClass().descriptorString());
         original.resetPose();
         original.root().setRotation(90.0F, 3.15F, 1.0F);
         return original;
