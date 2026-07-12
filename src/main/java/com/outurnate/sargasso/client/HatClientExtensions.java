99998d14b0eff81fbf028efceb4c903539cabc22
Outurnate <joseph@outurnate.com>
2026-07-12 15:50:00 +0000

    auto commit 1.0.391

--- src/main/java/com/outurnate/sargasso/client/HatClientExtensions.java
+++ src/main/java/com/outurnate/sargasso/client/HatClientExtensions.java
@@ -1,19 +1,20 @@
 package com.outurnate.sargasso.client;
 
 import net.minecraft.client.model.Model;
+import net.minecraft.client.model.player.PlayerModel;
 import net.minecraft.client.resources.model.EquipmentClientInfo;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 
 public class HatClientExtensions implements IClientItemExtensions {
+    @SuppressWarnings("rawtypes")
     @Override
     public Model getHumanoidArmorModel(
         ItemStack itemStack,
         EquipmentClientInfo.LayerType layerType,
         Model original) {
-        System.out.println(original.getClass().descriptorString());
-        original.resetPose();
-        original.root().setRotation(90.0F, 3.15F, 1.0F);
-        return original;
+        PlayerModel playerModel = (PlayerModel) original;
+        playerModel.getHead().skipDraw = true;
+        return playerModel;
     }
 }
