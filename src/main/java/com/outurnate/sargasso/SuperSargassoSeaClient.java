71d03f802d4e6f97d80ce978a8b7aeb9b312b9ae
Outurnate <joseph@outurnate.com>
2026-07-12 20:27:25 +0000

    auto commit 1.0.417

--- src/main/java/com/outurnate/sargasso/SuperSargassoSeaClient.java
+++ src/main/java/com/outurnate/sargasso/SuperSargassoSeaClient.java
@@ -62,7 +62,7 @@ public class SuperSargassoSeaClient {
                 .simpleModelWrapper(
                     Identifier.parse(LocalStandaloneModels.FOX_EARS.getName()),
                     new ComposedModelState(
-                        BlockModelRotation.get(OctahedralGroup.INVERT_Y),
+                        BlockModelRotation.get(OctahedralGroup.ROT_180_FACE_XY),
                         new Transformation(new Vector3f(0.0F, 1.0F, 0.0F), null, null, null))));
     }
 
