0beab6af52632b0a054040fb7768a4bacb141e7d
Outurnate <joseph@outurnate.com>
2026-07-23 23:54:18 +0000

    auto commit 1.0.541

--- src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
+++ src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
@@ -74,8 +74,12 @@ public class ShockTherapistEntityRenderer
     private static void lightning(LineSegment lineSegment, int depth, ArrayList<LineSegment> accumulator) {
         Vector3f segmentLength = lineSegment.end.sub(lineSegment.start).mul(0.5F);
         Vector3f midpoint = lineSegment.start.add(segmentLength).add(0.0F, 0.1F, 0.0F);
-        LineSegment segment1 = new LineSegment(lineSegment.start, midpoint);
-        LineSegment segment2 = new LineSegment(midpoint, lineSegment.end);
+        // LineSegment segment1 = new LineSegment(lineSegment.start, midpoint);
+        // LineSegment segment2 = new LineSegment(midpoint, lineSegment.end);
+        LineSegment segment1 = new LineSegment(lineSegment.start, lineSegment.end);
+        LineSegment segment2 = new LineSegment(
+            lineSegment.start.add(0.0F, 1.0F, 0.0F, midpoint),
+            lineSegment.end);
         if (depth == 0) {
             accumulator.add(segment1);
             accumulator.add(segment2);
