7ceb63dea4a0d4f33c9a78e3e8563e8ba09fbe08
Outurnate <joseph@outurnate.com>
2026-07-24 01:35:22 +0000

    auto commit 1.0.548

--- src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
+++ src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
@@ -139,12 +139,12 @@ public class ShockTherapistEntityRenderer
             poseStack.pushPose();
             Vec3 delta = mine.getPosition(0).subtract(state.blockPos.getCenter()); // TODO partial tick
             double length = delta.length();
-            poseStack.translate(0.5F, 0.5F, 0.5F);
             poseStack
                 .mulPose(Axis.YP.rotation((float) (-Math.atan2(delta.z, delta.x)) - (float) (Math.PI / 2)));
             poseStack.mulPose(
                 Axis.XP.rotation(
                     (float) (-Math.atan2(delta.horizontalDistance(), delta.y)) - (float) (Math.PI / 2)));
+            poseStack.translate(0.5F, 0.5F, 0.5F);
             submitCrystalBeams(
                 new LineSegment(
                     new Vec3(length, 0.0F, 0.0F),
