67010cbb5474003c454f2784756cba072773ea25
Outurnate <joseph@outurnate.com>
2026-07-25 19:28:57 +0000

    auto commit 1.0.599

--- src/main/java/com/outurnate/sargasso/client/LocalRenderTypes.java
+++ src/main/java/com/outurnate/sargasso/client/LocalRenderTypes.java
@@ -10,6 +10,7 @@ import com.outurnate.sargasso.SuperSargassoSea;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
 import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.resources.Identifier;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
@@ -36,9 +37,13 @@ public class LocalRenderTypes {
 
     public static final Identifier ZAP_LOCATION = SuperSargassoSea.ID("textures/entity/zap.png");
 
-    public static final RenderType ZAP = RenderType.create(
-        "zap",
-        RenderSetup.builder(RenderPipelines.ENTITY_SOLID)
-            .withTexture("Sampler0", ZAP_LOCATION)
-            .createRenderSetup());
+    public static final RenderType ZAP = RenderTypes.entitySolid(ZAP_LOCATION); /*
+                                                                                 * RenderType.create( "zap",
+                                                                                 * RenderSetup.builder(
+                                                                                 * RenderPipelines.
+                                                                                 * ENTITY_SOLID)
+                                                                                 * .withTexture("Sampler0",
+                                                                                 * ZAP_LOCATION)
+                                                                                 * .createRenderSetup());
+                                                                                 */
 }
