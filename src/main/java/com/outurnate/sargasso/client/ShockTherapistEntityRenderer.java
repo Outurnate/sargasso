18c418da4c051baf6147bce15db4a5448cfde50c
Outurnate <joseph@outurnate.com>
2026-07-25 01:53:08 +0000

    auto commit 1.0.576

--- src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
+++ src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
@@ -106,7 +106,7 @@ public class ShockTherapistEntityRenderer
 
     public static final Identifier ZAP_LOCATION = SuperSargassoSea.ID("textures/entity/zap.png");
 
-    private static final RenderType ZAP = RenderTypes.entitySolid(ZAP_LOCATION);
+    private static final RenderType ZAP = RenderTypes.entityTranslucent(ZAP_LOCATION);
 
     private static void lightning(
         LineSegment lineSegment,
