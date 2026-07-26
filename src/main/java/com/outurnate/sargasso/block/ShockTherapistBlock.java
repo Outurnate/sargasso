e46845dd7a1d0768264ee3920a444382cb47fb1a
Outurnate <joseph@outurnate.com>
2026-07-26 19:54:52 +0000

    auto commit 1.0.637

--- src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
+++ src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
@@ -74,7 +74,7 @@ public class ShockTherapistBlock extends DirectionalBlock implements EntityBlock
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
+        return this.defaultBlockState().setValue(FACING, context.getClickedFace().getOpposite());
     }
 
     @Override
