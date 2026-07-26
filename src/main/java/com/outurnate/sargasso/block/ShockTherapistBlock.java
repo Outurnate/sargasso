147302c8d004e1fe0f9897ac6fc6f0e6d0a0c96b
Outurnate <joseph@outurnate.com>
2026-07-26 19:23:34 +0000

    auto commit 1.0.633

--- src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
+++ src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
@@ -64,8 +64,7 @@ public class ShockTherapistBlock extends DirectionalBlock implements EntityBlock
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder);
-        builder.add(PHASE);
+        builder.add(FACING, PHASE);
     }
 
     @Override
