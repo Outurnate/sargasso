64c110113c63a818c5b9dc6bfba5d051fb467c31
Outurnate <joseph@outurnate.com>
2026-07-27 23:56:37 +0000

    auto commit 1.0.655

--- src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
+++ src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
@@ -20,6 +20,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -61,6 +62,34 @@ public class ShockTherapistBlock extends Block implements EntityBlock {
         BlockEntityType<E> checkedType,
         BlockEntityTicker<? super E> ticker) {
         return checkedType == type ? (BlockEntityTicker<A>) ticker : null;
+    }
+
+    private static VoxelShape defaultShape() {
+        VoxelShape shape = Shapes.empty();
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.25, 0.375, 0.6875, 0.5, 0.625), BooleanOp.OR);
+        shape = Shapes
+            .join(shape, Shapes.box(0.3125, 0.34375, 0.46875, 0.6875, 0.40625, 0.53125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.25, 0.25, 0.375, 0.3125, 0.5, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.6875, 0.25, 0.375, 0.75, 0.5, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.3125, 0.4375, 0.25, 0.4375, 0.5625), BooleanOp.OR);
+        shape = Shapes
+            .join(shape, Shapes.box(0.15625, 0.25, 0.46875, 0.21875, 0.3125, 0.53125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.1875, 0.4375, 0.25, 0.25, 0.5625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.75, 0.3125, 0.4375, 0.875, 0.4375, 0.5625), BooleanOp.OR);
+        shape = Shapes
+            .join(shape, Shapes.box(0.78125, 0.25, 0.46875, 0.84375, 0.3125, 0.53125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.75, 0.1875, 0.4375, 0.875, 0.25, 0.5625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.25, 0, 0.25, 0.75, 0.0625, 0.75), BooleanOp.OR);
+        shape = Shapes
+            .join(shape, Shapes.box(0.15625, 0.0625, 0.46875, 0.84375, 0.125, 0.53125), BooleanOp.OR);
+        shape = Shapes
+            .join(shape, Shapes.box(0.15625, 0.125, 0.46875, 0.21875, 0.1875, 0.53125), BooleanOp.OR);
+        shape = Shapes
+            .join(shape, Shapes.box(0.78125, 0.125, 0.46875, 0.84375, 0.1875, 0.53125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.375, 0.0625, 0.25, 0.625, 0.1875, 0.375), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.375, 0.0625, 0.6875, 0.4375, 0.25, 0.75), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.5, 0.0625, 0.625, 0.625, 0.1875, 0.75), BooleanOp.OR);
+        return shape;
     }
 
     public ShockTherapistBlock(Properties properties) {
@@ -125,7 +154,7 @@ public class ShockTherapistBlock extends Block implements EntityBlock {
         BlockGetter level,
         BlockPos pos,
         CollisionContext context) {
-        return Shapes.empty();
+        return defaultShape();
     }
 
     @Override
