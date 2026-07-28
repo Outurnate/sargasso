762de1c417569b903956984ec2bff55070acb688
Outurnate <joseph@outurnate.com>
2026-07-28 23:39:36 +0000

    auto commit 1.0.663

--- src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
+++ src/main/java/com/outurnate/sargasso/block/ShockTherapistBlock.java
@@ -64,8 +64,24 @@ public class ShockTherapistBlock extends Block implements EntityBlock {
         Shapes.box(0.125, 0.25, 0.0, 0.875, 0.75, 0.5),
         Direction.WEST,
         Shapes.box(0.5, 0.25, 0.125, 1.0, 0.75, 0.875));
-    private static final VoxelShape FLOOR_SHAPE = Shapes.box(0.125, 0.0, 0.25, 0.875, 0.5, 0.75);
-    private static final VoxelShape CEILING_SHAPE = Shapes.box(0.125, 0.5, 0.25, 0.875, 1.0, 0.75);
+    private static final Map<Direction, VoxelShape> FLOOR_SHAPE = Map.of(
+        Direction.NORTH,
+        Shapes.box(0.125, 0.0, 0.25, 0.875, 0.5, 0.75),
+        Direction.EAST,
+        Shapes.box(0.25, 0.0, 0.125, 0.75, 0.5, 0.875),
+        Direction.SOUTH,
+        Shapes.box(0.125, 0.0, 0.25, 0.875, 0.5, 0.75),
+        Direction.WEST,
+        Shapes.box(0.25, 0.0, 0.125, 0.75, 0.5, 0.875));
+    private static final Map<Direction, VoxelShape> CEILING_SHAPE = Map.of(
+        Direction.NORTH,
+        Shapes.box(0.125, 0.5, 0.25, 0.875, 1.0, 0.75),
+        Direction.EAST,
+        Shapes.box(0.125, 0.25, 0.0, 0.875, 0.75, 0.5),
+        Direction.SOUTH,
+        Shapes.box(0.125, 0.5, 0.25, 0.875, 1.0, 0.75),
+        Direction.WEST,
+        Shapes.box(0.125, 0.25, 0.0, 0.875, 0.75, 0.5));
 
     @SuppressWarnings("unchecked")
     private static <E extends BlockEntity, A extends BlockEntity> @Nullable BlockEntityTicker<A> createTickerHelper(
@@ -105,11 +121,11 @@ public class ShockTherapistBlock extends Block implements EntityBlock {
         BlockGetter level,
         BlockPos pos,
         CollisionContext context) {
-        return switch (state.getValue(ATTACH_FACE)) {
+        return (switch (state.getValue(ATTACH_FACE)) {
             case CEILING -> CEILING_SHAPE;
             case FLOOR -> FLOOR_SHAPE;
-            case WALL -> WALL_SHAPES.get(state.getValue(HORIZONTAL_FACING));
-        };
+            case WALL -> WALL_SHAPES;
+        }).get(state.getValue(HORIZONTAL_FACING));
         // return Shapes.box(0.125, 0.0, 0.25, 0.875, 0.5, 0.875);
     }
 
