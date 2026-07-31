8ac90639c539a78d228682eb6d839d94ef624997
Outurnate <joseph@outurnate.com>
2026-07-31 00:39:39 +0000

    auto commit 1.0.705

--- src/main/java/com/outurnate/sargasso/client/LocalRenderTypes.java
+++ src/main/java/com/outurnate/sargasso/client/LocalRenderTypes.java
@@ -10,6 +10,7 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 import com.outurnate.sargasso.SuperSargassoSea;
 
 import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.rendertype.OutputTarget;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.resources.Identifier;
@@ -62,6 +63,6 @@ public class LocalRenderTypes {
             .withTexture("Sampler0", ZAP_LOCATION)
             .affectsCrumbling()
             .sortOnUpload()
-            // .setOutputTarget(OutputTarget.ITEM_ENTITY_TARGET)
+            .setOutputTarget(OutputTarget.MAIN_TARGET)
             .createRenderSetup());
 }
