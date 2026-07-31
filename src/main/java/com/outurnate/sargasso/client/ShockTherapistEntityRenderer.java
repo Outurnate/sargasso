72f443052adff262f426fea9274475d793a36f8d
Outurnate <joseph@outurnate.com>
2026-07-31 00:15:22 +0000

    auto commit 1.0.699

--- src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
+++ src/main/java/com/outurnate/sargasso/client/ShockTherapistEntityRenderer.java
@@ -64,7 +64,7 @@ public class ShockTherapistEntityRenderer
                 depth,
                 segments,
                 random,
-                0.5F);
+                1.0F);
             this(origin, delta, segments);
         }
 
