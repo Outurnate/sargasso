cb17a3dafbb7126a2c39f41189d28e4d2e3f5110
Outurnate <joseph@outurnate.com>
2026-07-31 00:35:02 +0000

    auto commit 1.0.703

--- src/main/java/com/outurnate/sargasso/client/LocalRenderTypes.java
+++ src/main/java/com/outurnate/sargasso/client/LocalRenderTypes.java
@@ -46,9 +46,12 @@ public class LocalRenderTypes {
         .withShaderDefine("EMISSIVE")
         .buildSnippet();
 
-    public static final RenderPipeline ZAP_PIPELINE = RenderPipeline.builder(ZAP_SNIPPET)
-        .withLocation("pipeline/translucent_block")
-        .withShaderDefine("ALPHA_CUTOUT", 0.01F)
+    public static final RenderPipeline ZAP_PIPELINE = RenderPipeline
+        .builder(RenderPipelines.ENTITY_EMISSIVE_SNIPPET)
+        .withLocation("pipeline/entity_translucent_emissive")
+        .withShaderDefine("ALPHA_CUTOUT", 0.1F)
+        .withShaderDefine("PER_FACE_LIGHTING")
+        .withSampler("Sampler1")
         .withColorTargetState(new ColorTargetState(BlendFunction.TRANSLUCENT))
         .withCull(false)
         .withDepthStencilState(DepthStencilState.DEFAULT)
