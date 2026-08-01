13b0c94d9bb7e0df7e18ea900c6f9dd47359b8f1
Outurnate <joseph@outurnate.com>
2026-08-01 19:25:29 +0000

    auto commit 1.0.730

--- src/main/java/com/outurnate/sargasso/mixin/BlockStateBaseMixin.java
+++ src/main/java/com/outurnate/sargasso/mixin/BlockStateBaseMixin.java
@@ -6,8 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.RedStoneWireBlock;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour.BlockStateBase;
 import net.minecraft.world.level.redstone.Redstone;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -16,11 +15,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(RedStoneWireBlock.class)
-public abstract class RedstoneWireBlockMixin {
+@Mixin(BlockStateBase.class)
+public abstract class BlockStateBaseMixin {
     @Inject(method = "getSignal", at = @At("HEAD"), cancellable = true)
     private void sargasso$getSignal(
-        BlockState state,
         BlockGetter level,
         BlockPos pos,
         Direction direction,
