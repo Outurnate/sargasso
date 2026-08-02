087c9393589872797dc4da22be2c8d30f61df550
Outurnate <joseph@outurnate.com>
2026-08-02 23:13:04 +0000

    auto commit 1.0.786

--- src/main/java/com/outurnate/sargasso/datagen/LocalRecipeProvider.java
+++ src/main/java/com/outurnate/sargasso/datagen/LocalRecipeProvider.java
@@ -39,49 +39,41 @@ public class LocalRecipeProvider extends RecipeProvider {
 
     @Override
     protected void buildRecipes() {
-        this
-            .shaped(RecipeCategory.FOOD, LocalItems.BREADROCK)
+        shaped(RecipeCategory.FOOD, LocalItems.BREADROCK)
             .define('#', Items.WHEAT)
             .define('S', LocalItems.BEDROCK_SLOP.get())
             .pattern("###")
             .pattern("SSS")
             .unlockedBy("has_slop", this.has(LocalItems.BEDROCK_SLOP.get()))
             .save(this.output);
-        this
-            .shaped(RecipeCategory.BUILDING_BLOCKS, LocalItems.FLOTSAM)
+        shaped(RecipeCategory.BUILDING_BLOCKS, LocalItems.FLOTSAM)
             .define('J', LocalItems.DEBRIS.get())
             .pattern("JJ")
             .pattern("JJ")
             .unlockedBy("has_junk", this.has(LocalItems.DEBRIS.get()))
             .save(this.output);
-        this
-            .shapeless(RecipeCategory.TOOLS, LocalItems.TWO_COLOR_FOX_EARS)
+        shapeless(RecipeCategory.TOOLS, LocalItems.TWO_COLOR_FOX_EARS)
             .requires(LocalItems.AA_BATTERY)
             .requires(LocalItems.BLACK_FOX_EARS)
             .unlockedBy("has_fox_ears", this.has(LocalItems.BLACK_FOX_EARS.get()))
             .save(this.output);
-        this
-            .studdedLeatherSmithing(
-                Items.LEATHER_HELMET,
-                RecipeCategory.COMBAT,
-                LocalItems.STUDDED_LEATHER_HELMET.get());
-        this
-            .studdedLeatherSmithing(
-                Items.LEATHER_CHESTPLATE,
-                RecipeCategory.COMBAT,
-                LocalItems.STUDDED_LEATHER_CHESTPLATE.get());
-        this
-            .studdedLeatherSmithing(
-                Items.LEATHER_LEGGINGS,
-                RecipeCategory.COMBAT,
-                LocalItems.STUDDED_LEATHER_LEGGINGS.get());
-        this
-            .studdedLeatherSmithing(
-                Items.LEATHER_BOOTS,
-                RecipeCategory.COMBAT,
-                LocalItems.STUDDED_LEATHER_BOOTS.get());
-        this
-            .shaped(RecipeCategory.TOOLS, LocalItems.PERSONAL_VOLTMETER)
+        studdedLeatherSmithing(
+            Items.LEATHER_HELMET,
+            RecipeCategory.COMBAT,
+            LocalItems.STUDDED_LEATHER_HELMET.get());
+        studdedLeatherSmithing(
+            Items.LEATHER_CHESTPLATE,
+            RecipeCategory.COMBAT,
+            LocalItems.STUDDED_LEATHER_CHESTPLATE.get());
+        studdedLeatherSmithing(
+            Items.LEATHER_LEGGINGS,
+            RecipeCategory.COMBAT,
+            LocalItems.STUDDED_LEATHER_LEGGINGS.get());
+        studdedLeatherSmithing(
+            Items.LEATHER_BOOTS,
+            RecipeCategory.COMBAT,
+            LocalItems.STUDDED_LEATHER_BOOTS.get());
+        shaped(RecipeCategory.TOOLS, LocalItems.PERSONAL_VOLTMETER)
             .define('G', Items.GLASS_PANE)
             .define('C', Items.COPPER_NUGGET)
             .define('R', Items.REDSTONE)
@@ -91,6 +83,7 @@ public class LocalRecipeProvider extends RecipeProvider {
             .pattern("CRC")
             .unlockedBy("has_potato_battery", this.has(LocalItems.POTATO_BATTERY.get()))
             .save(this.output);
+        dyedItem(LocalItems.FOX_EARS.get(), "dyed_armor");
     }
 
     private void studdedLeatherSmithing(Item base, RecipeCategory category, Item result) {
