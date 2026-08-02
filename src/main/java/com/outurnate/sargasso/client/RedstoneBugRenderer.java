52e3f2a7c49447d55e3b37528745ffb3eedecf9e
Outurnate <joseph@outurnate.com>
2026-08-02 18:44:59 +0000

    auto commit 1.0.778

--- src/main/java/com/outurnate/sargasso/client/RedstoneBugRenderer.java
+++ src/main/java/com/outurnate/sargasso/client/RedstoneBugRenderer.java
@@ -34,7 +34,7 @@ public class RedstoneBugRenderer extends EntityRenderer<RedstoneBug, RedstoneBug
             3,
             0.5F,
             2.0F,
-            ARGB.color(255, 0, 0));
+            ARGB.color(100, 0, 0));
     }
 
     protected AABB getBoundingBoxForCulling(RedstoneBug entity) {
