63220b71cb0f9deaddc99d61f289fc8044a85cff
Outurnate <joseph@outurnate.com>
2026-08-08 19:21:00 +0000

    auto commit 1.0.890

--- src/main/java/com/outurnate/sargasso/worldgen/FloatingIslandFeature.java
+++ src/main/java/com/outurnate/sargasso/worldgen/FloatingIslandFeature.java
@@ -1,9 +1,13 @@
 package com.outurnate.sargasso.worldgen;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ChunkPos;
@@ -13,6 +17,9 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 
 public class FloatingIslandFeature extends Feature<NoneFeatureConfiguration> {
     public FloatingIslandFeature() {
@@ -27,19 +34,34 @@ public class FloatingIslandFeature extends Feature<NoneFeatureConfiguration> {
         int minX = genChunk.getMinBlockX();
         int targetY = context.origin().getY();
         int minZ = genChunk.getMinBlockZ();
+
         HashSet<BlockPos> surface = new HashSet<>();
+        ArrayList<BlockPos> centres = new ArrayList<>();
 
         int blobs = random.nextInt(2, 5);
         for (int i = 0; i < blobs; ++i) {
             int size = random.nextInt(5, 7);
             BlockPos origin = new BlockPos(minX + random.nextInt(16), targetY, minZ + random.nextInt(16));
+            centres.add(origin);
             placeIsland(surface, level, random, origin, size);
         }
-        int spikes = random.nextInt(0, 4);
+
+        int spikes = random.nextInt(blobs * 2, blobs * 3);
         for (int j = 0; j < spikes; ++j) {
             BlockPos origin = surface.stream().skip(random.nextInt(surface.size())).findFirst()
                 .orElse(context.origin());
             placeSpike(null, level, random, origin);
+        }
+
+        if (level instanceof WorldGenRegion region) {
+            if (region.getServer() instanceof MinecraftServer server) {
+                StructureTemplateManager structureManager = server.getStructureManager();
+                StructureTemplate template = structureManager.get(
+                    Identifier.fromNamespaceAndPath("minecraft", "village/snowy/houses/snowy_small_house_1"))
+                    .orElseThrow();
+                BlockPos centre = centres.get(random.nextInt(centres.size()));
+                template.placeInWorld(level, centre, centre, new StructurePlaceSettings(), random, 0);
+            }
         }
 
         return true;
