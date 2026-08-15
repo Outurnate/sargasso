d900f967736c42f2b0a64f33d5b07604875286bc
Outurnate <joseph@outurnate.com>
2026-08-15 22:40:55 +0000

    auto commit 1.0.954

--- src/main/java/com/outurnate/sargasso/client/ReinforcedStarMetalBlockEntityRenderer.java
+++ src/main/java/com/outurnate/sargasso/client/ReinforcedStarMetalBlockEntityRenderer.java
@@ -2,6 +2,7 @@ package com.outurnate.sargasso.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.outurnate.sargasso.SuperSargassoSea;
 import com.outurnate.sargasso.block.entity.ReinforcedStarMetalBlockEntity;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
@@ -48,6 +49,7 @@ public class ReinforcedStarMetalBlockEntityRenderer
         PoseStack poseStack,
         SubmitNodeCollector submitNodeCollector,
         CameraRenderState camera) {
+        SuperSargassoSea.LOGGER.error("ass");
         submitNodeCollector.submitCustomGeometry(
             poseStack,
             RenderTypes.armorEntityGlint(),
@@ -59,7 +61,7 @@ public class ReinforcedStarMetalBlockEntityRenderer
                 float z0 = 0;
 
                 float x1 = 1;
-                float y1 = 1;
+                float y1 = 2;
                 float z1 = 1;
 
                 vertex(vertexConsumer, matrix, x0, y0, z0, 0, 0);
