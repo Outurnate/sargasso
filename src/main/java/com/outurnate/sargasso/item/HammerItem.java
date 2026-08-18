8d28c65a26128d3b25d62cf2ea6d8093a4c0e45a
Outurnate <joseph@outurnate.com>
2026-08-18 23:34:30 +0000

    auto commit 1.0.989

--- src/main/java/com/outurnate/sargasso/item/HammerItem.java
+++ src/main/java/com/outurnate/sargasso/item/HammerItem.java
@@ -5,19 +5,18 @@ import com.outurnate.sargasso.entity.ThrownHammer;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Position;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.arrow.AbstractArrow;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.ProjectileItem;
 import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
@@ -41,51 +40,35 @@ public class HammerItem extends Item implements ProjectileItem {
     }
 
     @Override
-    public ItemUseAnimation getUseAnimation(ItemStack itemStack) {
-        return ItemUseAnimation.BOW;
-    }
-
-    @Override
-    public int getUseDuration(ItemStack itemStack, LivingEntity user) {
-        return 72000;
-    }
-
-    @Override
-    public boolean releaseUsing(ItemStack itemStack, Level level, LivingEntity entity, int remainingTime) {
-        if (entity instanceof Player player) {
-            Holder<SoundEvent> sound = EnchantmentHelper
-                .pickHighestLevel(itemStack, EnchantmentEffectComponents.TRIDENT_SOUND)
-                .orElse(SoundEvents.TRIDENT_THROW); // TODO hammer
-            if (level instanceof ServerLevel serverLevel) {
-                itemStack.hurtWithoutBreaking(1, player);
-                ItemStack thrownItemStack = itemStack.consumeAndReturn(1, player);
-                ThrownHammer hammer = Projectile.spawnProjectileFromRotation(
-                    ThrownHammer::new,
-                    serverLevel,
-                    thrownItemStack,
-                    player,
-                    0.0F,
-                    2.5F,
-                    1.0F);
-                if (player.hasInfiniteMaterials()) {
-                    hammer.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
-                }
-
-                level.playSound(null, hammer, sound.value(), SoundSource.PLAYERS, 1.0F, 1.0F);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
         ItemStack itemInHand = player.getItemInHand(hand);
-        if (itemInHand.nextDamageWillBreak()) {
+        if (player.getAttackStrengthScale(0.0F) < itemInHand
+            .getOrDefault(DataComponents.MINIMUM_ATTACK_CHARGE, 0.0F)) {
             return InteractionResult.FAIL;
-        } else {
-            player.startUsingItem(hand);
+        }
+
+        Holder<SoundEvent> sound = EnchantmentHelper
+            .pickHighestLevel(itemInHand, EnchantmentEffectComponents.TRIDENT_SOUND)
+            .orElse(SoundEvents.TRIDENT_THROW); // TODO hammer
+        if (level instanceof ServerLevel serverLevel) {
+            itemInHand.hurtWithoutBreaking(1, player);
+            ItemStack thrownItemStack = itemInHand.consumeAndReturn(1, player);
+            ThrownHammer hammer = Projectile.spawnProjectileFromRotation(
+                ThrownHammer::new,
+                serverLevel,
+                thrownItemStack,
+                player,
+                0.0F,
+                2.5F,
+                1.0F);
+            if (player.hasInfiniteMaterials()) {
+                hammer.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+            }
+
+            level.playSound(null, hammer, sound.value(), SoundSource.PLAYERS, 1.0F, 1.0F);
             return InteractionResult.CONSUME;
         }
+
+        return InteractionResult.PASS;
     }
 }
