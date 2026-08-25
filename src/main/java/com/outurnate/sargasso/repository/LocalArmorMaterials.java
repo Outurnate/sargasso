b98a70c2f1e2d8821a062dade7ebd0d977a0b4d9
Outurnate <joseph@outurnate.com>
2026-08-25 01:12:05 +0000

    auto commit 1.0.1065

--- src/main/java/com/outurnate/sargasso/repository/LocalArmorMaterials.java
+++ src/main/java/com/outurnate/sargasso/repository/LocalArmorMaterials.java
@@ -7,7 +7,6 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorType;
-import net.minecraft.world.item.equipment.EquipmentAssets;
 
 public class LocalArmorMaterials {
     public static final ArmorMaterial STUDDED_LEATHER = new ArmorMaterial(
@@ -18,7 +17,7 @@ public class LocalArmorMaterials {
         1.0F,
         0.0F,
         ItemTags.REPAIRS_LEATHER_ARMOR,
-        EquipmentAssets.LEATHER);
+        LocalEquipmentAssets.STUDDED_LEATHER);
 
     private static Map<ArmorType, Integer> makeDefense(int boots, int legs, int chest, int helm, int body) {
         return Maps.newEnumMap(
