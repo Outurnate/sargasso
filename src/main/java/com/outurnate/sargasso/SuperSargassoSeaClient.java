e569402fe7a89190ed71fd112ce5714c10ba1854
Outurnate <joseph@outurnate.com>
2026-08-28 19:06:19 +0000

    auto commit 1.0.1080

--- src/main/java/com/outurnate/sargasso/SuperSargassoSeaClient.java
+++ src/main/java/com/outurnate/sargasso/SuperSargassoSeaClient.java
@@ -46,7 +46,7 @@ public class SuperSargassoSeaClient {
 
     @SubscribeEvent
     public static void registerColorHandlers(RegisterColorHandlersEvent.BlockTintSources event) {
-        event.register(List.of(state -> ARGB.color(10, 245, 10)), LocalBlocks.ALPHA_GRASS.get());
+        event.register(List.of(state -> ARGB.color(110, 200, 70)), LocalBlocks.ALPHA_GRASS.get());
     }
 
     @SubscribeEvent
