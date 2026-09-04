8e07854949d0e3cae3b42f01abeffdea1db49229
Outurnate <joseph@outurnate.com>
2026-09-04 01:48:04 +0000

    auto commit 1.0.1129

--- src/datagen/java/com/outurnate/sargasso/datagen/worldgen/LocalStructureTemplatePoolsProvider.java
+++ src/datagen/java/com/outurnate/sargasso/datagen/worldgen/LocalStructureTemplatePoolsProvider.java
@@ -156,29 +156,30 @@ public class LocalStructureTemplatePoolsProvider extends StructureProvider {
         bootstrap.register(
             VILLAGE_BUILDINGS_A,
             entry("village/building_1", LocalStructureProcessorListProvider.WOOLS),
-            entry("village/building_2", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_3", LocalStructureProcessorListProvider.WOOLS.size()),
+            entry("village/building_7", LocalStructureProcessorListProvider.WOOLS),
+            entry("village/building_15", LocalStructureProcessorListProvider.WOOLS),
             entry("village/building_4", LocalStructureProcessorListProvider.WOOLS.size()),
             entry("village/building_arena", LocalStructureProcessorListProvider.WOOLS.size()));
         bootstrap.register(
             VILLAGE_BUILDINGS_B,
-            entry("village/building_5", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_6", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_7", LocalStructureProcessorListProvider.WOOLS),
-            entry("village/building_8", LocalStructureProcessorListProvider.WOOLS.size()));
+            entry("village/building_2"),
+            entry("village/building_5"),
+            entry("village/building_6"),
+            entry("village/building_8"),
+            entry("village/building_prison"));
         bootstrap.register(
             VILLAGE_BUILDINGS_C,
-            entry("village/building_9", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_10", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_11", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_12", LocalStructureProcessorListProvider.WOOLS.size()));
+            entry("village/building_9"),
+            entry("village/building_10"),
+            entry("village/building_11"),
+            entry("village/building_12"));
         bootstrap.register(
             VILLAGE_BUILDINGS_D,
-            entry("village/building_13", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_library", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_15", LocalStructureProcessorListProvider.WOOLS),
-            entry("village/building_16", LocalStructureProcessorListProvider.WOOLS.size()),
-            entry("village/building_17", LocalStructureProcessorListProvider.WOOLS.size()));
+            entry("village/building_13"),
+            entry("village/building_3"),
+            entry("village/building_library"),
+            entry("village/building_16"),
+            entry("village/building_vat"));
         bootstrap.register(VILLAGE_WALLS, "village/wall");
         bootstrap.register(VILLAGE_CORNERS, "village/wall_corner");
         bootstrap.register(VILLAGE_PATH_FOUNDATIONS, "village/path_foundation");
